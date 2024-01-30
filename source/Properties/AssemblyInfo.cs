943cd6d9a992aee98b654883cfa6ac41eea3213b
Zafer Balkan <zafer@zaferbalkan.com>
2024-01-30 21:02:53 +0200

    Updated version and documentation

--- source/Properties/AssemblyInfo.cs
+++ source/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Info-Assure")]
 [assembly: AssemblyProduct("dumpntsd")]
-[assembly: AssemblyCopyright("Copyright © Info-Assure 2015")]
+[assembly: AssemblyCopyright("Copyright © Info-Assure 2015, © Zafer Balkan 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
